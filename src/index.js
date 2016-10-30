5321b63250b66b770c8ca60f3633d92e8b1da408
Caique Caleiro <caiquecaleiro@gmail.com>
2016-10-30 17:04:45 -0200

    Add eslint configuration and Search Bar component

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/SearchBar';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
